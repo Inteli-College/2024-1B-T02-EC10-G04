282e24bcff784c607c3581a0de65e3adec33f738
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2024-05-08 11:06:04 -0300

    fix: medice entity

--- backend/internal/domain/entity/medicine.go
+++ backend/internal/domain/entity/medicine.go
@@ -4,25 +4,35 @@ import (
 	"time"
 )
 
-type PyxisRepository interface {
-	CreatePyxis(pyxis *Pyxis) (*Pyxis, error)
-	FindPyxisById(id string) (*Pyxis, error)
-	FindAllPyxis() ([]*Pyxis, error)
-	UpdatePyxis(pyxis *Pyxis) (*Pyxis, error)
-	DeletePyxis(id string) error
+type StripeType string
+
+const (
+	StripeRed    StripeType = "red"
+	StripeYellow StripeType = "yellow"
+	StripeBlack  StripeType = "black"
+)
+
+type MediceRepository interface {
+	CreateMedice(medicine *Medicine) (*Medicine, error)
+	FindMediceById(id string) (*Medicine, error)
+	FindAllMedices() ([]*Medicine, error)
+	UpdateMedice(pyxis *Pyxis) (*Medicine, error)
+	DeleteMedice(id string) error
 }
 
-type Medicines struct {
-	ID        string    `json:"id" db:"id"`
-	Batch     string    `json:"label" db:"label"`
-  Name string `json:"name" db:"name"`
-  Stripe 
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+type Medicine struct {
+	ID        string     `json:"id" db:"id"`
+	Batch     string     `json:"label" db:"label"`
+	Name      string     `json:"name" db:"name"`
+	Stripe    StripeType `json:"stripe" db:"stripe"`
+	CreatedAt time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at" db:"updated_at"`
 }
 
-func NewPyxis(label string) *Pyxis {
-	return &Pyxis{
-		Label:     label,
+func NewMedice(batch string, name string, stipe StripeType) *Medicine {
+	return &Medicine{
+		Batch:  batch,
+		Name:   name,
+		Stripe: stripe,
 	}
 }
