eb0a14bbf815c858708edc8fbdc55aeb092f54ca
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2024-05-08 10:24:27 -0300

    fix: database folder created and ngix folder moved to deployments

--- backend/internal/domain/entity/medicine.go
+++ backend/internal/domain/entity/medicine.go
@@ -1,1 +1,28 @@
 package entity
+
+import (
+	"time"
+)
+
+type PyxisRepository interface {
+	CreatePyxis(pyxis *Pyxis) (*Pyxis, error)
+	FindPyxisById(id string) (*Pyxis, error)
+	FindAllPyxis() ([]*Pyxis, error)
+	UpdatePyxis(pyxis *Pyxis) (*Pyxis, error)
+	DeletePyxis(id string) error
+}
+
+type Medicines struct {
+	ID        string    `json:"id" db:"id"`
+	Batch     string    `json:"label" db:"label"`
+  Name string `json:"name" db:"name"`
+  Stripe 
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+func NewPyxis(label string) *Pyxis {
+	return &Pyxis{
+		Label:     label,
+	}
+}
