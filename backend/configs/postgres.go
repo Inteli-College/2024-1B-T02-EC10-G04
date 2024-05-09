58167d547a59a1ae60915246e873bba58a0acc9a
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2024-05-09 11:56:24 -0300

    fix: db env variable

--- backend/configs/postgres.go
+++ backend/configs/postgres.go
@@ -1,14 +1,15 @@
 package config
 
 import (
-	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
 	"log"
 	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func SetupPostgres() *sqlx.DB {
-	connStr := os.Getenv("POSTGRES_URL")
+	connStr := os.Getenv("DB_URL")
 	if connStr == "" {
 		log.Fatal("DB_URL is not set")
 	}
