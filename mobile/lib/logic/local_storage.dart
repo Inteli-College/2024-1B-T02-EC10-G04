a4037c00da27e219cbd1c6ac24e9f53a2c871417
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2024-05-23 22:42:45 -0300

    fix: localstorage

--- mobile/lib/logic/local_storage.dart
+++ mobile/lib/logic/local_storage.dart
@@ -3,19 +3,25 @@ import 'package:shared_preferences/shared_preferences.dart';
 class LocalStorageService {
   // Save a value to local storage
   Future<void> saveValue(String key, String value) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(key, value);
+    Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+    await prefs.then((pref) {
+      pref.setString(key, value);
+    });
   }
 
   // Retrieve a value from local storage
   Future<String?> getValue(String key) async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(key);
+    Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+    return prefs.then((pref) {
+      pref.getString(key);
+    });
   }
 
   // Remove a value from local storage
   Future<void> removeValue(String key) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(key);
+    Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+    await prefs.then((pref) {
+      pref.remove(key);
+    });
   }
 }
