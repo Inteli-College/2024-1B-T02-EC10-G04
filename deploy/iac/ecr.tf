fd8580522edb8c65e51b75b2b4440f44358532f1
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2024-06-01 17:23:11 -0300

    feat: deploy aws

---
+++ deploy/iac/ecr.tf
@@ -0,0 +1,10 @@
+resource "aws_ecr_repository" "backend_ecr" {
+  name                 = "backend"
+
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
