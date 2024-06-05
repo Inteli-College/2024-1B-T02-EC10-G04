e66385bd009a027cab5486ceb9e86823ab96327e
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2024-06-04 23:09:18 -0300

    fix: build images

--- deploy/iac/sg.tf
+++ deploy/iac/sg.tf
@@ -1,28 +1,3 @@
-resource "aws_security_group" "bastion_sg" {
-  name        = "bastion_sg"
-  description = "Security group for Bastion Host"
-  vpc_id      = aws_vpc.prod_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    name     = "bastion_sg"
-    menageBy = "Terraform"
-  }
-}
-
 resource "aws_security_group" "private_sg" {
   name        = "Private_SG_ArqCorp"
   description = "Security group for Private Host"
