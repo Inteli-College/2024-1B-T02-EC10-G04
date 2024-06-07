fe8d808a93be7dc00a9ca5f08d74ec1a1aca325c
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2024-06-07 10:56:51 -0300

    fix: redis

--- deploy/iac/redis.tf
+++ deploy/iac/redis.tf
@@ -13,19 +13,17 @@ resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   }
 }
 
-resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "redis-cluster-prod"
-  engine               = "redis"
-  engine_version       = "6.x"
+resource "aws_elasticache_replication_group" "redis" {
+  replication_group_id = "redis-replication-group"
+  description          = "redis with authentication"
   node_type            = "cache.t3.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.x"
+  num_cache_clusters   = 1
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
   security_group_ids   = [aws_security_group.redis_sg.id]
-  auth_token = "myredispassword"
-  tags = {
-    name     = "redis-cluster-prod"
-    menageBy = "Terraform"
-  }
+  parameter_group_name = "default.redis6.x"
+  engine_version       = "6.x"
+
+  transit_encryption_enabled = true
+  auth_token                 = "abcdefgh1234567890"
 }
